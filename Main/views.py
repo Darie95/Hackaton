2f990706dc190d038b0690034ecdfe77b2e4612b
Dmitry Datsyk <dmitry-datsyk@yandex.by>
2018-07-11 19:55:23 +0300

    first view

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from Main.models import Expense
+
+
+def expense(request):
+    exp = Expense.objects.all()
+    return render(request, 'index.html', {'exp': exp})
--- hackaton/urls.py
+++ hackaton/urls.py
@@ -23,6 +23,7 @@ from hackaton import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.expense ),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
